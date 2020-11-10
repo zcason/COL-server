f7dac6674e0766b66cc2aff4e4c86dbc71ef3e74
zcason <zuri.cason@gmail.com>
2020-11-10 16:43:39 -0500

    truncate data tables

--- seeds/seed.col_tables.sql
+++ seeds/seed.col_tables.sql
@@ -9,8 +9,8 @@ VALUES
 INSERT INTO col_events (title, event_desc, event_date, user_id)
 VALUES
 ('Mary''s Concert', 'At the Scope, bring roses.', '2021-1-5', 1),
-('Martketing Pitch Meeting', 'Look over pitch material.', '2020-12-18', 6),
-('Pick Dry Cleaning', null, '2020-11-23', 4),
-('15th Anniversary', 'Pick up gift from the Store', '2020-11-28', 5),
-('Lunch with Clients', null, '2020-12-15', 6),
-('Boxing Match', 'On PPV', '2020-12-8', 4);
+('Martketing Pitch Meeting', 'Look over pitch material.', '2020-12-18', 3),
+('Pick Dry Cleaning', null, '2020-11-23', 1),
+('15th Anniversary', 'Pick up gift from the Store', '2020-11-28', 2),
+('Lunch with Clients', null, '2020-12-15', 3),
+('Boxing Match', 'On PPV', '2020-12-8', 1);
