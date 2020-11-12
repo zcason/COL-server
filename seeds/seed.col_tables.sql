57ceba8d04993530d5247aa17350a5e57eb298f1
zcason <zuri.cason@gmail.com>
2020-11-12 09:59:59 -0500

    updated seed

--- seeds/seed.col_tables.sql
+++ seeds/seed.col_tables.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO col_users (email, full_name, password)
 VALUES
 ('john.doe@gmail.com', 'John Doe', 'johnpassword1'),
@@ -8,9 +7,9 @@ VALUES
 
 INSERT INTO col_events (title, event_desc, event_date, user_id)
 VALUES
-('Mary''s Concert', 'At the Scope, bring roses.', '2021-1-5', 1),
-('Martketing Pitch Meeting', 'Look over pitch material.', '2020-12-18', 3),
-('Pick Dry Cleaning', null, '2020-11-23', 1),
-('15th Anniversary', 'Pick up gift from the Store', '2020-11-28', 2),
-('Lunch with Clients', null, '2020-12-15', 3),
-('Boxing Match', 'On PPV', '2020-12-8', 1);
+('Mary''s Concert', 'At the Scope, bring roses.', '2021-1-5 01:30:00', 1),
+('Martketing Pitch Meeting', 'Look over pitch material.', '2020-12-18 18:00:00', 3),
+('Pick Dry Cleaning', null, '2020-11-23 16:15:00', 1),
+('15th Anniversary', 'Pick up gift from the Store', '2020-11-28 23:00:00', 2),
+('Lunch with Clients', null, '2020-12-15 17:30:00', 3),
+('Boxing Match', 'On PPV', '2020-12-8 00:00:00', 1);
