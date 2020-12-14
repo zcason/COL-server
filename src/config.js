bac40b3e0c7c9b4778389110af8c5fd939cf3ba4
zcason <zuri.cason@gmail.com>
2020-12-14 08:24:50 -0600

    deployment changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://col_admin@localhost:5432/cog_offloader',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://col_admin@localhost:5432/cog_offloader',
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://col_admin@localhost:5432/cog_offloader-test',
     JWT_SECRET: process.env.JWT_SECRET || 'offloading-86531'
 }
