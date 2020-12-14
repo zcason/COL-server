03e2cf7823dd45d6066c703d35246e2248c4441e
zcason <zuri.cason@gmail.com>
2020-12-14 10:46:14 -0600

    update

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
-    "connectionString": (process.env.NODE_ENV === 'production')
-        ? process.env.DATABASE_URL
-        : process.env.TEST_DATABASE_URL,
-    "ssl": true,
+    "connectionString": (process.env.NODE_ENV === 'test')
+        ? process.env.TEST_DATABASE_URL
+        : process.env.DATABASE_URL,
 }
