374840d882290d5558aba26213bc714432b7b133
zcason <zuri.cason@gmail.com>
2020-12-14 16:56:24 -0600

    added change seed file data

--- seeds/seed.col_tables.sql
+++ seeds/seed.col_tables.sql
@@ -5,9 +5,9 @@ VALUES
 
 INSERT INTO col_events (title, event_desc, event_date, user_id)
 VALUES
-('Mary''s Concert', 'At the Scope, bring roses.', '2021-1-5 01:30:00', 1),
-('Martketing Pitch Meeting', 'Look over pitch material.', '2020-12-18 18:00:00', 1),
-('Pick Dry Cleaning', null, '2020-11-23 16:15:00', 1),
-('15th Anniversary', 'Pick up gift from the Store', '2020-11-28 23:00:00', 1),
-('Lunch with Clients', null, '2020-12-15 17:30:00', 1),
-('Boxing Match', 'On PPV', '2020-12-8 00:00:00', 1);
+('Mary''s Concert', 'At the Scope, bring roses.', '2021-1-5 01:30:00', 2),
+('Martketing Pitch Meeting', 'Look over pitch material.', '2020-12-18 18:00:00', 2),
+('Pick Dry Cleaning', null, '2020-11-23 16:15:00', 2),
+('15th Anniversary', 'Pick up gift from the Store', '2020-11-28 23:00:00', 2),
+('Lunch with Clients', null, '2020-12-15 17:30:00', 2),
+('Boxing Match', 'On PPV', '2020-12-8 00:00:00', 2);
